a4132d7030ad82a829445eb0926a7c4ed5eac369
chinafei <liuyafei.com@qq.com>
2016-03-21 16:12:49 +0800

    init aliyun 0.2

--- saltsatck_aliyun/files/shell.sh
+++ saltsatck_aliyun/files/shell.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 ntpdate time-a.nist.gov
 hwclock --systohc
-crontab -e  << EOF
+crontab -u root -e  << EOF
 0 0 * * * /usr/sbin/ntpdate time-a.nist.gov
 EOF
 
-#mount -a
+
+#disk=/dev/sdb
+disk=/dev/vdb
+fdisk $disk << EOF
+n
+p
+1
+1
+
+w
+EOF
+mkfs.ext4 ${disk}1
+echo '/dev/vdb1  /mnt ext4    defaults    0  0' >> /etc/fstab
+mount -a
 
 #file end
